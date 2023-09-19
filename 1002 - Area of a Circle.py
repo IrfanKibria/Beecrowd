4f2df39d71e35a1fae4a470954f4512ffb0867d1
IrfanKibria <irfan.py3@gmail.com>
2023-09-19 10:21:37 +0600

    This Is First Version Of Problem solving

---
+++ 1002 - Area of a Circle.py
@@ -0,0 +1,6 @@
+n = float(input())
+R = float(3.14159)
+
+A = R*n**2
+
+print("A=%0.4f" %A)
---
+++ 1003 - Simple Sum.py
@@ -0,0 +1,17 @@
+a = int(input())
+
+b = int(input())
+
+sum = a+b
+
+print(f"SOMA = {sum}")
+
+
+
+a = int(input())
+
+b = int(input())
+
+sum = a+b
+
+print("SOMA = %d" %sum)
