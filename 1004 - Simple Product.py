43ed36d8fac01286611fd382aaa817a44c6320ef
IrfanKibria <irfan.py3@gmail.com>
2023-09-24 10:04:57 +0600

    First version

---
+++ 1004 - Simple Product.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+
+Mul = a*b
+
+print("PROD = %d" %Mul)
+
+
+
+
+a = float(input())
+b = float(input())
+
+Mul = a*b
+
+print(f"PROD = {Mul}")
---
+++ 1005 - Average 1.py
@@ -0,0 +1,7 @@
+a = float(input())
+
+b = float(input())
+
+avg = float(((a*3.5)+ (b*7.5))/11)
+
+print(f"MEDIA = {avg:0.5f}")
